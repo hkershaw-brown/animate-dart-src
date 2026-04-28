6a06826c4b7bd8505ca5146bbaf36381539d7d5d
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-04-28 16:50:25 -0400

    portrait nice

--- run_g_mac_users.sh
+++ run_g_mac_users.sh
@@ -10,14 +10,18 @@ gource DART \
 --hide mouse,progress,filenames,files \
 -1080x1280 \
 --output-ppm-stream  - \
---highlight-users \
---max-files 600 \
+--max-files 400 \
 --output-framerate 60 \
 --disable-bloom \
---user-scale 1.04 \
 --dir-name-depth 3 \
 --file-filter work \
 --file-filter shell_scripts \
 --font-size 40 \
+--file-idle-time 10 \
+--user-scale 0.8 \
+--fixed-user-size \
+--highlight-users \
 | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 -b 65536K dart.mp4
 
+# --user-scale 1.04 \
+# --fixed-user-size \
