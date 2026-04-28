e9b5794a3978c0e7e9a433942579b8a030dc3de9
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-04-28 15:42:33 -0400

    making animations

---
+++ run_g_mac_users.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+gource DART \
+-c 4 \
+-s 0.03 \
+--disable-progress \
+--date-format "%d/%m/%y" \
+--disable-progress \
+--stop-at-end \
+--hide mouse,progress,filenames \
+-1280x720 \
+--output-ppm-stream  - \
+--highlight-users \
+--highlight-dirs \
+--max-files 600 \
+--output-framerate 60 \
+| ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 -b 65536K dart.mp4
+
