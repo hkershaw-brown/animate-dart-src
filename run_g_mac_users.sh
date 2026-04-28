11c9a6b737a9ec8b040d6fc5d707ade460cb8fff
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-04-28 15:56:46 -0400

    people, dirs no bloom

--- run_g_mac_users.sh
+++ run_g_mac_users.sh
@@ -8,11 +8,12 @@ gource DART \
 --disable-progress \
 --stop-at-end \
 --hide mouse,progress,filenames \
--1280x720 \
+-1920x1080 \
 --output-ppm-stream  - \
 --highlight-users \
---highlight-dirs \
 --max-files 600 \
 --output-framerate 60 \
+--disable-bloom \
+--user-scale 1.04 \
 | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 -b 65536K dart.mp4
 
